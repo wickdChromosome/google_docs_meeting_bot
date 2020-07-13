b0c0296acc1d2d609f702cab2b51d46c33b5932b
wickdchromosome <bkotis@scripps.edu>
2020-07-13 09:55:46 -0700

    Added more options

--- src/meeting_poll.py
+++ src/meeting_poll.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import requests
 from io import StringIO
 import argparse
+import numpy as np
 
 def pull_schedule(file_id):
     '''Downloads the group schedule from google drive'''
@@ -23,7 +24,13 @@ def parse_tsv(pulled_schedule_str, speaker_col, topics_col):
     next_session = f[f.index > datetime.now()].head(1)
 
     speaker = next_session[next_session.columns[speaker_col]].tolist()[0]
-    options = next_session[next_session.columns[topics_col]].tolist()[0].split(",")
+
+    #check if there is actually a list of items in that cell, instead of just nan
+    optionsitem = next_session[next_session.columns[topics_col]].tolist()[0] 
+    if type(optionsitem) != float:
+        options = optionsitem.split(",")
+    else:
+        options = []
 
     return([speaker, options])
 
@@ -40,7 +47,7 @@ def create_message(next_session):
     [speaker, options] = next_session
 
     message1 = "`This week's speaker is: " + speaker + "!`\n"
-    message = message1 + "*What should this week's journal club presentation be about? Available topics are:*\n\n"
+    message = message1 + "*What should this week's journal club presentation be about?*\n\n"
 
     options_list = []
 
@@ -48,13 +55,13 @@ def create_message(next_session):
     #if there is only a single option, don't make a poll
     if num_options == 1:
 
-        full_message = message + "Looks like there is only one offered talk this week: *" + options[0] + "*\n"
+        full_message = message1 + "\n*Looks like there is only one offered journal club presentation this week: " + options[0] + "*\n"
         return(full_message)
 
     #if there are no options, send a different message
     if num_options == 0:
 
-        full_message = message + "Looks like there are no offered talks this week.\n"
+        full_message = message1 + "\n*Looks like there are no offered talks this week.*\n"
         return(full_message)
 
     for i in range(len(options)):
@@ -73,15 +80,15 @@ def main():
     parser = argparse.ArgumentParser(description='Parses a Google Sheets file containing a meeting schedule')
     parser.add_argument('--webhook_url', required=True, type=str, help='Webhook URL from Slack')
     parser.add_argument('--drive_file_id', required=True, type=str, help='File ID on Google Drive')
-    parser.add_argument('--speaker_col', required=True, type=str, help='Zero based index for the column containing the speaker names in the dataset')
-    parser.add_argument('--presentation_col', required=True, type=str, help='Zero based index for the column containing the presentation options in the dataset')
+    parser.add_argument('--speaker_col', required=True, type=int, help='Zero based index for the column containing the speaker names in the dataset')
+    parser.add_argument('--topics_col', required=True, type=int, help='Zero based index for the column containing the presentation options in the dataset')
     args = parser.parse_args()
 
     print("Webhook URL:" + args.webhook_url)
     print("Google Drive file ID:" + args.drive_file_id)
 
     schedule = pull_schedule(args.drive_file_id)
-    next_session = parse_tsv(schedule, speaker_col=args.speaker_col, presentation_col=args.presentation_col)
+    next_session = parse_tsv(schedule, speaker_col=args.speaker_col, topics_col=args.topics_col)
     msg = create_message(next_session)
     send_message(msg, args.webhook_url)
     print(msg)
